3606c432d3cdc272efe139cda21685148ff79a86
Bill Sheng <billxsheng@gmail.com>
2019-07-01 01:05:04 -0400

    testing

--- streaming-analytics-consumer/src/main/java/Main.java
+++ streaming-analytics-consumer/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import spark.SparkKafkaConnect;
+
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,8 +15,6 @@ public class Main {
     public static void main(String[] args) throws StreamingQueryException, InterruptedException {
         Logger.getLogger("org")
                 .setLevel(Level.OFF);
-
-
         SparkKafkaConnect.connectToTopic();
 
 //        SparkSession spark = SparkSession.builder().config("spark.master", "local").getOrCreate();
@@ -24,10 +26,11 @@ public class Main {
 //                .load();
 //        lines.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
 //
-//        val rdd = rddRaw.map(_.value.toString);
-//        val df = spark.read.json(rdd);
+//        List<String> list = lines.as(Encoders.STRING()).collectAsList();
+//        Dataset<String> df1 = spark.createDataset(list, Encoders.STRING());
 //
 //
+//        Dataset<Row> tweets = lines.map(df1);
 //        query.awaitTermination();
     }
 }
--- streaming-analytics-consumer/src/main/java/spark/SparkKafkaConnect.java
+++ streaming-analytics-consumer/src/main/java/spark/SparkKafkaConnect.java
@@ -6,21 +6,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 import models.Tweet;
+import org.apache.commons.codec.StringDecoder;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaInputDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
 import twitter4j.Status;
 
 public class SparkKafkaConnect {
     public static void connectToTopic() throws InterruptedException {
+        SparkSession spark = SparkSession.builder().getOrCreate();
         SparkConf sparkConf = new SparkConf().setAppName("spark-streaming").setMaster("local[2]").set("spark.executor.memory","1g");
         sparkConf.setAppName("WordCountingApp");
         sparkConf.set("spark.cassandra.connection.host", "127.0.0.1");
@@ -41,9 +43,13 @@ public class SparkKafkaConnect {
                         LocationStrategies.PreferConsistent(),
                         ConsumerStrategies.<String, String> Subscribe(topics, kafkaParams));
 
-//        JavaDStream<String> tweets = tweetStream.map(tweet -> tweet.value());
+        tweetStream.map(record -> {
+            System.out.println("fdsafdsa");
+            return record.value();
+        }).print();
 
-        JavaDStream<Tweet> tweets = tweetStream.map(status -> buildNewTweet(status));
+
+//        JavaDStream<String> tweets = tweetStream.map(tweet -> tweet.value());
 //        tweets.foreachRDD(rdd -> {
 //            for(String r: rdd.collect()) {
 //                System.out.println("*" + r);
@@ -52,9 +58,5 @@ public class SparkKafkaConnect {
 
         streamingContext.start();
         streamingContext.awaitTermination();
-    }
-
-    public static Tweet buildNewTweet(Status status) {
-
     }
 }
