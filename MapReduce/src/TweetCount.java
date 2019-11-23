862f63c70b0b8c4a856caf1d41e97818c6beac98
Bill Sheng <billxsheng@gmail.com>
2019-11-23 13:23:00 -0500

    Skeleton Method Functionality

--- MapReduce/src/TweetCount.java
+++ MapReduce/src/TweetCount.java
@@ -1,3 +1,21 @@
-public class TweetCount {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
 
+public class TweetCount {
+    //Map Cassandra DB rows into (location, 1)
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+    }
+
+    //Reduce to (location, sum(values))
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
