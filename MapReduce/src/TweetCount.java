d89da5b5610db0ad2b615496780b8a2192e81dcd
Bill Sheng <billxsheng@gmail.com>
2019-11-24 13:06:33 -0500

    Map Function

--- MapReduce/src/TweetCount.java
+++ MapReduce/src/TweetCount.java
@@ -6,8 +6,22 @@ import org.apache.hadoop.util.*;
 
 public class TweetCount {
     //Map Cassandra DB rows into (location, 1)
+    //Angle brackets include input/output
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        //Hadoop Integer, optimized to provide serialization in Hadoop
+        //Serialization is the process of converting an object into a stream of bytes to store
+        private final static IntWritable one = new IntWritable(1);
+        private Text location = new Text();
 
+        //Map lines into key value of (location, one)
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                location.set(tokenizer.nextToken());
+                output.collect(location, one);
+            }
+        }
     }
 
     //Reduce to (location, sum(values))
